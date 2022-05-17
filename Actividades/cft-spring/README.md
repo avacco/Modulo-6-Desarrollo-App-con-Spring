e90aaacbe451dd21213184834cedf97214549d3e
Andres Vargas <a.m.vargas1996@gmail.com>
2022-05-17 19:52:10 -0300

    Reorganizacion

--- Actividades/cft-spring/README.md
+++
@@ -1,2 +0,0 @@
-## Trabajo en clases
-Adaptar trabajo [CFT-WEB](https://github.com/avacco/Modulo-5-Desarrollo-web-Java/tree/main/Actividades/cft-web) del modulo anterior a Spring.
--- Actividades/tesoreria-jpa/src/main/java/cl/andres/java/tesoreria/repository/ProveedorRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.andres.java.tesoreria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cl.andres.java.tesoreria.modelo.Proveedor;
-
-public interface ProveedorRepository extends JpaRepository<Proveedor, Long>{
-
-}
