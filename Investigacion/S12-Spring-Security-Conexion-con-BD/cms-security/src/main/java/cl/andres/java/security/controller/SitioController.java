a39025180e1d21ba09ae2f0b82906d77bb753edc
Andres Vargas <a.m.vargas1996@gmail.com>
2022-05-23 23:16:12 -0300

    Sesion 12: Creacion y autenticacion de usuario a traves de BD

---
+++ Investigacion/S12-Spring-Security-Conexion-con-BD/cms-security/src/main/java/cl/andres/java/security/controller/SitioController.java
@@ -0,0 +1,24 @@
+package cl.andres.java.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SitioController {
+
+	@GetMapping("/")
+	public String inicio() {
+		return "inicio";
+	}
+	
+	@GetMapping("/nosotros")
+	public String nosotros() {
+		return "nosotros";
+	}
+	
+	@GetMapping("/contacto")
+	public String contacto() {
+		return "contacto";
+	}
+	
+}
